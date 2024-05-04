565fdd9e89b2bae07704a4dc800245c2aa79d606
PauAdell <pau.adell1@gmail.com>
2024-05-04 18:08:32 +0200

    added icons to interests

--- app/src/modules/TripOverlap.tsx
+++ app/src/modules/TripOverlap.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import InterestIconComponent from 'src/modules/InterestIconComponent';
 
 interface TripOverlapProps {
     name: string;
@@ -8,13 +9,18 @@ interface TripOverlapProps {
     overlapDays: string[];
 }
 
+//const overlaptrips = [
+//    { "name": "Juan Alberto", "profilePicture" : "https://as1.ftcdn.net/v2/jpg/02/01/33/54/1000_F_201335438_CNpY0iWaXXAV95Gj8BPB0tEJlMcxWeaZ.jpg", "interests" : ["sports", "music"], "dates" : [ "23/06/2024" , "25/06/2024"]},
+//    { "name": "Maria Luisa", "profilePicture" : "https://as1.ftcdn.net/v2/jpg/02/01/33/54/1000_F_201335438_CNpY0iWaXXAV95Gj8BPB0tEJlMcxWeaZ.jpg", "interests" : ["museums", "music"], "dates" : ["16/06/2024", "21/06/2024"]},
+//];
+
 const TripOverlapComponent = ({ name, profilePicture, interests, overlapDays}: TripOverlapProps) => {
     return (
         <View style={styles.container}>
             <View style={styles.textContainer}>
                 <Text style={styles.name}>{name}</Text>
-                <Text style={styles.interests}>{interests.join(', ')}</Text>
-                <Text style={styles.overlap}>Days in city: {overlapDays.length}</Text>
+                <InterestIconComponent interests={interests} />
+                <Text style={styles.overlap}>From: {overlapDays[0] + " to " + overlapDays[1]}</Text>
             </View>
             <Image source={{ uri: profilePicture }} style={styles.profileImage} />
         </View>
