8b0ac09d5cf93587a05ecf2597ce5c742b36f427
fer7G <fernand07gn@gmail.com>
2024-05-05 02:04:14 +0200

    New navigation buttons style

--- app/src/pages/TripPage.tsx
+++ app/src/pages/TripPage.tsx
@@ -1,38 +1,79 @@
-import React from 'react';
-
-import { FontAwesome } from '@expo/vector-icons';
-
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, SafeAreaView } from 'react-native';
 import TripDetail from 'src/pages/TripDetail';
 import TripOverlaps from './TripOverlaps';
 
-const Tab = createBottomTabNavigator();
-
-function TripPage({ route, navigation }) {
+function TripPage({ route }) {
     const { trip } = route.params;
+    const [active, setActive] = useState('Info');
+
+    const renderComponent = () => {
+        switch (active) {
+            case 'Info':
+                return <TripDetail trip={trip} />;
+            case 'Networking':
+                return <TripOverlaps trip={trip} />;
+            default:
+                return null;
+        }
+    };
 
     return (
-        <Tab.Navigator screenOptions={
-            ({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName = route.name === 'Info' ? 'info-circle' : 'users';
-                    let iconColor = focused ? 'tomato' : 'gray';
-
-                    return <FontAwesome name={iconName} size={24} color={iconColor} />;
-                },
-                tabBarActiveTintColor: 'tomato',
-                tabBarInactiveTintColor: 'gray',
-                headerShown: false,
-            })}>
-            <Tab.Screen name="Info">
-                {() => <TripDetail trip={trip} />}
-            </Tab.Screen>
-            <Tab.Screen name="Networking">
-                {() => <TripOverlaps trip={trip} />}
-            </Tab.Screen>
-        </ Tab.Navigator>
+        <SafeAreaView style={styles.container}>
+            {renderComponent()}
+            <View style={styles.navBar}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setActive('Info')}>
+                    <Text style={styles.buttonText}>Info</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setActive('Networking')}>
+                    <Text style={styles.buttonText}>Networking</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    navBar: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        position: 'absolute',  // Posición absoluta para mantener los botones siempre visibles
+        bottom: 0,             // Alinea la barra de navegación en la parte inferior de la pantalla
+        width: '100%',         // Asegura que la barra se extienda a lo largo de toda la pantalla
+        backgroundColor: 'transparent',
+        paddingVertical: 60,
+        paddingHorizontal: 30,
+    },
+    button: {
+        backgroundColor: '#d9d9d9',
+        paddingHorizontal: 20,
+        paddingVertical: 12,
+        borderRadius: 30,
+        marginRight: 15,
+        // Añade estas líneas para la sombra
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    buttonText: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+});
+
 export default TripPage;
+
+
